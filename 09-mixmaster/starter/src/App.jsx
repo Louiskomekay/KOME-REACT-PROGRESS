92d612b6b5ceff3b6502fec9ee9bf40ee0bfd6ac
Louiskome <okwoho.kome@gmail.com>
2023-08-27 10:21:48 +0100

    Latest update

--- 09-mixmaster/starter/src/App.jsx
+++ 09-mixmaster/starter/src/App.jsx
@@ -1,10 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { About, HomeLayout, Landingpage, Error, Cocktail, Newsletter, SinglePageError } from './pages';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { loader as landingLoader } from './pages/Landingpage';
 import { loader as singleCocktailLoader } from './pages/Cocktail';
 import { action as newsletterAction } from "./pages/Newsletter";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    }
+  }
+})
 
 const router = createBrowserRouter([
   {
@@ -16,7 +25,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Landingpage />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: 'about',
@@ -25,7 +34,7 @@ const router = createBrowserRouter([
       {
         path: 'cocktail/:id',
         errorElement: <SinglePageError />,
-        loader: singleCocktailLoader,
+        loader: singleCocktailLoader(queryClient),
         element: <Cocktail />
       },
       {
@@ -38,6 +47,11 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 };
 export default App;
